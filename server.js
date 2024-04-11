ff1b405df7aecd46f3c2705bb5cb173da0327a53
Srikaar <srikarkoreddy9@gamail.com>
2024-04-11 11:05:29 +0530

    map is also working

--- server.js
+++ server.js
@@ -5,16 +5,25 @@ dotenv.config({ path: "./config.env" });
 const app = require("./app.js");
 const DB = process.env.DATABASE;
 
-mongoose.connect(DB).then(() => {
-  console.log("Database connected SuccessFully !!");
-});
+const connectDB = async () => {
+  try {
+    mongoose.connect(DB).then(() => {
+      console.log("Database connected SuccessFully !!");
+    });
+  } catch (error) {
+    console.log("error");
+    process.exit(1);
+  }
+};
 
-let port = process.env.PORT;
+let port = process.env.PORT || 3000;
 // app.use((req, res, next) => {
 //   console.log("sfdsfasdf");
 //   next();
 // });
 
-app.listen(port, () => {
-  console.log(`listening to the port ${port}`);
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log("listening for requests");
+  });
 });
